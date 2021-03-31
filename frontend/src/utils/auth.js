1e57ad1ad59e653de759a27db52cd66d2ce628b4
Anton Khomchenko <khomchenko.anton@yandex.ru>
2021-03-31 18:11:30 +0300

    stable 1.1 / url fixed

--- frontend/src/utils/auth.js
+++ frontend/src/utils/auth.js
@@ -1,4 +1,4 @@
-export const BASE_URL = `${window.location.protocol}${process.env.REACT_APP_API_URL || '//localhost:3001'}`;
+export const BASE_URL = `https://vskipel-backend.nomoredomains.icu/`;
 
 export const register = (password, email) => {
   return fetch(`${BASE_URL}/signup`, {
